9bfd4b4e43084116640d38b2ba65f7e188fb9bad
Harsh Mevada <harsh@Harsh.localdomain>
2026-05-20 11:58:19 +0000

    tested pipeline

--- bezkoder-ui/src/App.js
+++ bezkoder-ui/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <Link to={"/tutorials"} className="navbar-brand">
-            bezKoder
+            devops pipeline working successfully
           </Link>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
